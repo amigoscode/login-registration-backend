f2d5aa6c38d9f9212986d155f7c9e4646354ab2c
Saranya1994 <saranyabangarus@oakland.edu>
2022-04-10 13:04:05 -0400

    zipcontroller
    all working files-Chef Menu,Details

--- src/main/java/ethniconnect_backend/Zip/ZipController.java
+++ src/main/java/ethniconnect_backend/Zip/ZipController.java
@@ -1,12 +1,14 @@
 package ethniconnect_backend.Zip;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin()
 @RestController
 public class ZipController {
     @Autowired
