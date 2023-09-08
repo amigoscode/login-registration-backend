5ea2a687c2237b338abecd06c61bab139e15c2f5
Spunei Artur <arthurspunei@gmail.com>
2023-09-08 18:14:03 +0300

    replaced deprecated WebMvcConfigurerAdapter

--- src/main/java/com/example/tx/controller/DevCorsConfiguration.java
+++ src/main/java/com/example/tx/controller/DevCorsConfiguration.java
@@ -1,26 +1,21 @@
 package com.example.tx.controller;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 @Profile("development")
-public class DevCorsConfiguration {
+public class DevCorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("//**") // Specify your endpoint patterns
-                        .allowedOrigins("http://localhost:9000") // Allow requests from your frontend development server
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
-                        .allowCredentials(true) // Allow cookies and authentication headers
-                        .maxAge(3600); // Set the maximum age (in seconds) of the preflight response
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // Adjust the development frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
--- src/main/java/com/example/tx/controller/ProdCorsConfiguration.java
+++ src/main/java/com/example/tx/controller/ProdCorsConfiguration.java
@@ -1,27 +1,22 @@
 package com.example.tx.controller;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 @Profile("production")
-public class ProdCorsConfiguration {
+public class ProdCorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Specify your endpoint patterns
-                        .allowedOrigins("https://domain.com") // Allow requests from your production frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
-                        .allowCredentials(true) // Allow cookies and authentication headers
-                        .maxAge(3600); // Set the maximum age (in seconds) of the preflight response
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://your-production-frontend.com") // Adjust the production frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
